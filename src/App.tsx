a30788ea3e68dbe57f702559ea128f61207812d5
kaliganoff <kaliganoff@yahoo.com>
2024-07-19 19:08:48 +0300

    fix: Delete unnecessary things, add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
 
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import Main from "../../components/Main/Main";
 import getData from "../../services/getData";
